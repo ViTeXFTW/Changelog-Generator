e939345d15c27bb551d0ffa34e53e8a170372a02
ViTeXFTW <mads213@gmail.com>
2025-03-16 13:13:55 +0100

    fix(constants): wrong repo name in constants.py

--- src/constants.py
+++ src/constants.py
@@ -7,7 +7,7 @@ load_dotenv()
 # Access the GitHub token from the environment
 GH_TOKEN = os.getenv("GH_TOKEN")
 
-REPO_NAME = "ViTeXFTW/GeneralsGameCode"
+REPO_NAME = "ViTeXFTW/Changelog-Generator"
 RELEASE_BRANCH = "main"
 
 CHANGELOG_INITIAL_CONTENT = "# Changelog\n\nAll notable changes to this project will be documented in this file.\n\nThe format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).\n\n"
